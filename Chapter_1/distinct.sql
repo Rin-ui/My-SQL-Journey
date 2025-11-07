f6b9f7e546f550d3b47d8cbda63319b6f28a5d70
Rajashri Roy Choudhury <rajashri.biology@gmail.com>
2025-11-07 22:36:07 +0530

    distinct.sql

--- Chapter_1/distinct.sql
+++ Chapter_1/distinct.sql
@@ -1,4 +1,4 @@
--- return unique lists of all countries
+-- return unique lists of all countries ( distinct use to columns with duplicate values to remove duplicates)
 SELECT DISTINCT
 country
 FROM customers
