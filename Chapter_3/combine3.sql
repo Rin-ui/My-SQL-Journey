98fee3d4c82ff438c5eb4916939ab94cc22a8c7f
Rajashri Roy Choudhury <rajashri.biology@gmail.com>
2026-01-02 17:29:47 +0530

    combine3.sql

---
+++ Chapter_3/combine3.sql
@@ -0,0 +1,33 @@
+-- orders are stored in seperate tables (orders and order archive). combine all orders into one report without duplicate
+SELECT
+'Order' AS SourceTable, -- source flag
+	   [OrderID]
+      ,[ProductID]
+      ,[CustomerID]
+      ,[SalesPersonID]
+      ,[OrderDate]
+      ,[ShipDate]
+      ,[OrderStatus]
+      ,[ShipAddress]
+      ,[BillAddress]
+      ,[Quantity]
+      ,[Sales]
+      ,[CreationTime]
+FROM Sales.Orders
+UNION
+SELECT 
+'OrderArchive' AS SourceTable,
+       [OrderID]
+      ,[ProductID]
+      ,[CustomerID]
+      ,[SalesPersonID]
+      ,[OrderDate]
+      ,[ShipDate]
+      ,[OrderStatus]
+      ,[ShipAddress]
+      ,[BillAddress]
+      ,[Quantity]
+      ,[Sales]
+      ,[CreationTime]
+FROM Sales.OrdersArchive
+ORDER BY OrderID
