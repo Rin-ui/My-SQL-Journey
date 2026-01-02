acdc4edcac48f806169f447ec13d8f349146021a
Rajashri Roy Choudhury <rajashri.biology@gmail.com>
2026-01-02 17:12:49 +0530

    combine2.sql

---
+++ Chapter_3/combine2.sql
@@ -0,0 +1,31 @@
+-- avoid using stars (*) and rather list the columns as in many times all columns of both tables might not be identical
+-- orders are stored in seperate tables (orders and order archive). combine all orders into one report without duplicate
+SELECT
+[OrderID]
+      ,[ProductID]
+      ,[CustomerID]
+      ,[SalesPersonID]
+      ,[OrderDate]
+      ,[ShipDate]
+      ,[OrderStatus]
+      ,[ShipAddress]
+      ,[BillAddress]
+      ,[Quantity]
+      ,[Sales]
+      ,[CreationTime]
+FROM Sales.Orders
+UNION
+SELECT 
+[OrderID]
+      ,[ProductID]
+      ,[CustomerID]
+      ,[SalesPersonID]
+      ,[OrderDate]
+      ,[ShipDate]
+      ,[OrderStatus]
+      ,[ShipAddress]
+      ,[BillAddress]
+      ,[Quantity]
+      ,[Sales]
+      ,[CreationTime]
+FROM Sales.OrdersArchive
